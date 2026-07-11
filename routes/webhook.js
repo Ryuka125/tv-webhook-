fcf4c0c35c0de4c1a6153505296da27d448e78b9
Ryuka125 <78072874+Ryuka125@users.noreply.github.com>
2026-07-12 01:11:30 +0700

    Log webhook reception in console

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,12 +2,11 @@ module.exports = (app) => {
 
     app.post("/webhook", (req, res) => {
 
+        console.log("Webhook diterima:");
         console.log(req.body);
 
         res.json({
-
             success: true
-
         });
 
     });
