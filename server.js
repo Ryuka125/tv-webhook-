a16547fd3c8cc2febc168e36970b55c703bc4b6a
Ryuka125 <78072874+Ryuka125@users.noreply.github.com>
2026-07-11 23:13:52 +0700

    Implement EMA calculations and update status response
    Added EMA calculations and trend determination to the status endpoint.

--- server.js
+++ server.js
@@ -8,7 +8,18 @@ const PORT = process.env.PORT || 3000;
 
 let lastPrice = 0;
 let candles = [];
+let ema20 = 0;
+let ema50 = 0;
+let trend = "SIDEWAYS";
 let connected = false;
+
+if (ema20 > ema50) {
+    trend = "BUY";
+} else if (ema20 < ema50) {
+    trend = "SELL";
+} else {
+    trend = "SIDEWAYS";
+}
 
 // Website
 app.get("/", (req, res) => {
@@ -18,11 +29,14 @@ app.get("/", (req, res) => {
 // Status bot
 app.get("/status", (req, res) => {
     res.json({
-        status: connected ? "CONNECTED" : "DISCONNECTED",
-        symbol: "BTCUSDT",
-        price:lastPrice,
-        candles:candles.length
-    });
+    status: connected ? "CONNECTED" : "DISCONNECTED",
+    symbol: "BTCUSDT",
+    price: lastPrice,
+    candles: candles.length,
+    ema20,
+    ema50,
+    trend
+  });
 });
 
 // Koneksi WebSocket Binance
@@ -43,6 +57,14 @@ function loadCandles() {
             res.on("end", () => {
 
                 candles = JSON.parse(data);
+                const closes = candles.map(c => Number(c[4]));
+
+                ema20 = calculateEMA(20, closes);
+
+                ema50 = calculateEMA(50, closes);
+
+                console.log("EMA20 :", ema20);
+                console.log("EMA50 :", ema50);
 
                 console.log(
                     "Candles berhasil diambil:",
@@ -88,6 +110,18 @@ ws.on("error", (err) => {
 
 app.listen(PORT, () => {
     loadCandles();
+    function calculateEMA(period, closes) {
+
+    const multiplier = 2 / (period + 1);
+
+    let ema = closes[0];
+
+    for (let i = 1; i < closes.length; i++) {
+        ema = (closes[i] - ema) * multiplier + ema;
+    }
+
+    return ema;
+         }
 
 setInterval(loadCandles,60000);
     console.log("Server berjalan di port", PORT);
