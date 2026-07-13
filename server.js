d19f9fc89804e61212f0b3e0e6bd25625861a507
Ryuka125 <78072874+Ryuka125@users.noreply.github.com>
2026-07-13 14:13:31 +0700

    Integrate market, order, and websocket services

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 
+const marketService = require("./services/marketService");
+
+const orderService = require("./services/orderService");
+
+const websocketService = require("./services/websocketService");
+
 const webhookRoute = require("./routes/webhook");
 
 const app = express();
@@ -11,6 +17,10 @@ app.use(express.json());
 app.use("/", webhookRoute);
 
 const PORT = process.env.PORT || 8080;
+
+marketService.startMarket();
+
+websocketService.startSocket();
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
