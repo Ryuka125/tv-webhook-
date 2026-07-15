b263050f55afff290b25be18ad094bee8d368010
Ryuka125 <78072874+Ryuka125@users.noreply.github.com>
2026-07-15 10:11:29 +0700

    Integrate status update on WebSocket connection

--- services/websocketService.js
+++ services/websocketService.js
@@ -1,9 +1,21 @@
+const status = require("./statusService");
+
 const logger = require("../utils/logger");
 
 function startSocket() {
+
+    status.updateState({
+
+        websocket: "CONNECTED"
+
+    });
+
     logger.info("WebSocket Connected");
+
 }
 
 module.exports = {
+
     startSocket
+
 };
